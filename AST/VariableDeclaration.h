28b552640e712f71ec7429dedfa9f4be5972ac4c
hborkows <hubert.borkowski@hotmail.com>
2019-05-05 17:52:00 +0200

    Added VariableDeclaration

--- AST/VariableDeclaration.h
+++ AST/VariableDeclaration.h
@@ -5,12 +5,21 @@
 #ifndef TKOM_INTERPRETER_VARIABLEDECLARATION_H
 #define TKOM_INTERPRETER_VARIABLEDECLARATION_H
 
-
+#include <string>
 #include "Lvalue.h"
+#include "Assignable.h"
 
 class VariableDeclaration: public Lvalue
 {
+public:
+    void setName(std::string name) {this->name = name;}
 
+    void setValue(Assignable* value) {this->value = value;}
+
+    Type getType() const override {return ASTNode::Type::var_declaration;}
+protected:
+    Assignable* value;
+    std::string name;
 };
 
 
