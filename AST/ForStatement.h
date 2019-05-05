894c3fe751f9b82850c21d32cbe5760f3f2c5011
hborkows <hubert.borkowski@hotmail.com>
2019-05-05 18:51:22 +0200

    Added ForStatement

--- AST/ForStatement.h
+++ AST/ForStatement.h
@@ -7,10 +7,24 @@
 
 
 #include "ASTNode.h"
+#include "Condition.h"
+#include "StatementBlock.h"
 
 class ForStatement: public ASTNode
 {
+public:
+    ForStatement() = default;
 
+    ~ForStatement() {delete block; delete condition;}
+
+    void setBlock(StatementBlock* block) {this->block = block;}
+
+    void setCondition(Condition* cond) {condition = cond;}
+
+    Type getType() const override {return ASTNode::Type::for_statement;}
+protected:
+    StatementBlock* block;
+    Condition* condition;
 };
 
 
