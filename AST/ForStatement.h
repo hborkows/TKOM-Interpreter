e76c2a61b47faa0b18532def9f50aba585a1c658
hborkows <hubert.borkowski@hotmail.com>
2019-05-05 18:56:56 +0200

    Added WhileStatement

--- AST/ForStatement.h
+++ AST/ForStatement.h
@@ -9,22 +9,27 @@
 #include "ASTNode.h"
 #include "Condition.h"
 #include "StatementBlock.h"
+#include "Variable.h"
 
 class ForStatement: public ASTNode
 {
 public:
     ForStatement() = default;
 
-    ~ForStatement() {delete block; delete condition;}
+    ~ForStatement() {delete block; delete condition; delete variable;}
 
     void setBlock(StatementBlock* block) {this->block = block;}
 
     void setCondition(Condition* cond) {condition = cond;}
 
+    void setVariable(Variable* var) {variable = var;}
+
     Type getType() const override {return ASTNode::Type::for_statement;}
+
 protected:
     StatementBlock* block;
     Condition* condition;
+    Variable* variable;
 };
 
 
--- AST/WhileStatement.h
+++ AST/WhileStatement.h
@@ -7,10 +7,25 @@
 
 
 #include "ASTNode.h"
+#include "StatementBlock.h"
+#include "Condition.h"
 
 class WhileStatement: public ASTNode
 {
+public:
+    WhileStatement() = default;
 
+    ~WhileStatement() {delete block; delete condition;}
+
+    void setBlock(StatementBlock* block) {this->block = block;}
+
+    void setCondition(Condition* cond) {condition = cond;}
+
+    Type getType() const override {return ASTNode::Type::while_statement;}
+
+protected:
+    StatementBlock* block;
+    Condition* condition;
 };
 
 
