166823a55a7b47e9a7e02b5c87f7809809d65a4a
hborkows <hubert.borkowski@hotmail.com>
2019-05-05 19:06:34 +0200

    Added Condition

--- AST/Condition.h
+++ AST/Condition.h
@@ -7,10 +7,25 @@
 
 
 #include "Assignable.h"
+#include "../Lexer.h"
 
 class Condition: public Assignable
 {
+public:
+    Condition() = default;
 
+    void setNegated(bool flag) {negated = flag};
+
+    void setOperation(const LexType& operation) {this->operation = operation;}
+
+    void addOperand(ASTNode* operand) {operands.push_back(operand);}
+
+    Type getType() const override {return ASTNode::Type::condition;}
+
+protected:
+    bool negated;
+    LexType operation;
+    std::vector<ASTNode*> operands;
 };
 
 
