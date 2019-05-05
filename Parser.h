463d7985bde0d850644b6c366fff8753de53ab44
hborkows <hubert.borkowski@hotmail.com>
2019-05-05 13:20:18 +0200

    Added Tracer class

---
+++ Parser.h
@@ -0,0 +1,15 @@
+//
+// Created by hborkows on 05.05.19.
+//
+
+#ifndef TKOM_INTERPRETER_PARSER_H
+#define TKOM_INTERPRETER_PARSER_H
+
+
+class Parser
+{
+
+};
+
+
+#endif //TKOM_INTERPRETER_PARSER_H
