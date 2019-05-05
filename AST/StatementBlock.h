761db6c01530fe9a6a5ea491fca566336a2176d4
hborkows <hubert.borkowski@hotmail.com>
2019-05-05 19:12:28 +0200

    Added StatementBlock

--- AST/StatementBlock.h
+++ AST/StatementBlock.h
@@ -5,12 +5,21 @@
 #ifndef TKOM_INTERPRETER_STATEMENTBLOCK_H
 #define TKOM_INTERPRETER_STATEMENTBLOCK_H
 
-
+#include <vector>
 #include "ASTNode.h"
 
 class StatementBlock: public ASTNode
 {
+public:
+    void addStatement(ASTNode* statement) {statements.push_back(statement);}
 
+    void setLevel(size_t level) {this->level = level;}
+
+    Type getType() const override {return ASTNode::Type::block;}
+
+protected:
+    std::vector<ASTNode*> statements;
+    size_t level;
 };
 
 
