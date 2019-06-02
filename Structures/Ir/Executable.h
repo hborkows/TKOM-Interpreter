cc5356097274fe8c69247a3320847206a844d6ad
hborkows <hubert.borkowski@hotmail.com>
2019-06-02 11:37:41 +0200

    Added Executable class

---
+++ Structures/Ir/Executable.h
@@ -0,0 +1,19 @@
+//
+// Created by hborkows on 02.06.19.
+//
+
+#ifndef TKOM_INTERPRETER_EXECUTABLE_H
+#define TKOM_INTERPRETER_EXECUTABLE_H
+
+#include <unordered_map>
+
+class Scope;
+class Function;
+
+class Executable
+{
+    virtual void execute(Scope* scope, std::unordered_map<std::string, Function*> functions) = 0;
+};
+
+
+#endif //TKOM_INTERPRETER_EXECUTABLE_H
---
+++ Structures/Ir/Function.h
@@ -0,0 +1,15 @@
+//
+// Created by hborkows on 02.06.19.
+//
+
+#ifndef TKOM_INTERPRETER_FUNCTION_H
+#define TKOM_INTERPRETER_FUNCTION_H
+
+
+class Function
+{
+
+};
+
+
+#endif //TKOM_INTERPRETER_FUNCTION_H
